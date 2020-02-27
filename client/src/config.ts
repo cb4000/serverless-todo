657fec23606ff38c975237d39c60ba86f3e55917
Charles Blount <charles.blount@gmail.com>
2020-02-26 21:26:45 -0500

    Added front end.

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'kwr3vzp1tc'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-w24-md91.auth0.com',
+  clientId: 'ky8C2V30OTcgbtN1kfd6t1CubNp0AIA7',
+  callbackUrl: 'http://localhost:3000/callback'
+}
